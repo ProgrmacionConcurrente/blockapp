2c789ed7f19e6afdf6e93326d1ab92c1bff2791b
Julissa Ponte <u201713771@upc.edu.pe>
2022-06-29 16:27:32 -0500

    Fix save transaction service

--- src/main/java/com/blockapp/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/blockapp/service/impl/TransactionServiceImpl.java
@@ -1,12 +1,15 @@
 package com.blockapp.service.impl;
 
 import com.blockapp.entity.Transaction;
+import com.blockapp.entity.User;
 import com.blockapp.repository.TransactionRepository;
+import com.blockapp.repository.UserRepository;
 import com.blockapp.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,10 +19,24 @@ import java.util.Optional;
 public class TransactionServiceImpl implements TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     @Transactional
     public Transaction save(Transaction transaction) throws Exception {
+        User sender = transaction.getSender();
+        BigDecimal amountS = sender.getAmount();
+        amountS = amountS.subtract(transaction.getAmount());
+        sender.setAmount(amountS);
+        userRepository.save(sender);
+
+        User receiver = transaction.getReceiver();
+        BigDecimal amountR = receiver.getAmount();
+        amountR = amountR.add(transaction.getAmount());
+        receiver.setAmount(amountR);
+        userRepository.save(receiver);
+
         return transactionRepository.save(transaction);
     }
 
